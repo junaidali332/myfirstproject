b4be26efe52814ffa54dee2b3235f3cfb17fe9d0
junaidali332 <149886729+junaidali332@users.noreply.github.com>
2023-11-04 20:58:02 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+     document.getElementById("main").style.marginLeft = "0";
+    
+  }
+
+  // .NET CARDS
+
+  
